c9ea790bb2a276c6a93e8e1cecaa1f404908f1ef
DESKTOP-C9HBIR9\Bevis <add412@gmail.com>
2025-10-25 01:22:37 +0800

    <modify>：改良gmap模組 - 調整gmap模組內的函式 - 新增gmap_search函式：簡單的搜尋並回傳結果（未經處理的資訊） - 調整place_dict的欄位（增加types和website欄位） - 調整get_place_dict的參數項目，可自動適配多種可能情形

--- mods/gmap.py
+++ mods/gmap.py
@@ -63,13 +63,11 @@ API 訪問次數為每月限額（實際次數有點混亂但應該夠我們使
 """
 
 
-
-
 def get_key(api_key_path):
     """提供key檔案路徑，自動讀取內容並回傳"""
     with open(api_key_path, "r", encoding="UTF-8") as f:
         api_key = f.read()
-    
+
     return api_key
 
 
@@ -79,21 +77,9 @@ def trans_unix_to_date(timestamp):
     dt = datetime.fromtimestamp(timestamp)
 
     # 格式化成 "年-月-日"
-    date_str = dt.strftime('%Y-%m-%d')
+    date_str = dt.strftime("%Y-%m-%d")
 
     return date_str
-
-
-def get_place_id(api_key: str, key_word: str):
-    """提供關鍵字（最好是店名+地址），透過gmap搜尋取得place_id"""
-    gmaps = googlemaps.Client(key=api_key)
-    search_result = gmaps.places(query=key_word, language='zh-TW')
-    if len(search_result["results"]) != 0:
-        place_id = search_result.get("results", {})[0]["place_id"]
-        return place_id
-    else:
-        return None
-
 
 
 def newest_review_date(review_list: list):
@@ -107,119 +93,182 @@ def newest_review_date(review_list: list):
     return date
 
 
-def gmap_info(ori_name, api_key, place_id):
+def gmap_search(api_key, place_id=None, keyword=None):
+    gmaps = googlemaps.Client(key=api_key)
+
+    if place_id is not None:
+        search_result = gmaps.place(place_id=place_id, language="zh-TW")
+        return search_result
+    elif keyword is not None:
+        search_result = gmaps.places(query=keyword, language="zh-TW")
+        return search_result
+    else:
+        raise TypeError("請輸入 Place ID 或 keyword")
+
+
+def get_place_id(api_key: str, keyword: str):
+    """提供關鍵字（最好是店名+地址），透過gmap搜尋取得搜尋結果的第一個place_id"""
+    gmaps = googlemaps.Client(key=api_key)
+    search_result = gmaps.places(query=keyword, language="zh-TW")
+    if len(search_result["results"]) != 0:
+        place_id = search_result.get("results", {})[0]["place_id"]
+        return place_id
+    else:
+        return None
+
+
+def gmap_info(ori_name, api_key, place_id=None):
     """提供place_id，回傳名稱、營業狀態、營業時間、gmap評分、經緯度、gmap網址、最新評論日期"""
-    if place_id != None:
+    if place_id is not None:
         gmaps = googlemaps.Client(key=api_key)
-        detail = gmaps.place(place_id=place_id, language='zh-TW')
+        detail = gmaps.place(place_id=place_id, language="zh-TW")
         name = detail.get("result", {}).get("name", None)
         business_status = detail.get("result", {}).get("business_status", None)
 
         if detail["result"]["formatted_address"]:
             address = detail.get("result", {}).get("formatted_address", None)
-        elif detail['result']['adr_address']:
-            address = BeautifulSoup(detail.get('result', {}).get('adr_address', None), "html.parser").text
+        elif detail["result"]["adr_address"]:
+            address = BeautifulSoup(
+                detail.get("result", {}).get("adr_address", None), "html.parser"
+            ).text
         else:
             address = None
-        
+
         phone = detail.get("result", {}).get("formatted_phone_number", None)
-        if phone != None:
+        if phone is not None:
             phone = phone.replace(" ", "")
 
-        opening_hours = detail.get("result", {}).get("opening_hours", {}).get("weekday_text", None)
-        rating = detail.get("result", {}).get('rating', None)
+        opening_hours = (
+            detail.get("result", {}).get("opening_hours", {}).get("weekday_text", None)
+        )
+        types = detail.get("result", {}).get("types", None)
+        rating = detail.get("result", {}).get("rating", None)
         rating_total = detail.get("result", {}).get("user_ratings_total", None)
-        # geocode = if_exist(detail["result"]['geometry']['location'])
-        longitude = detail.get("result", {}).get('geometry', {}).get('location', {}).get('lng', None)
-        latitude = detail.get("result", {}).get('geometry', {}).get('location', {}).get('lat', None)
+        longitude = (
+            detail.get("result", {})
+            .get("geometry", {})
+            .get("location", {})
+            .get("lng", None)
+        )
+        latitude = (
+            detail.get("result", {})
+            .get("geometry", {})
+            .get("location", {})
+            .get("lat", None)
+        )
         map_url = detail.get("result", {}).get("url", None)
-        review_list = detail.get("result", {}).get('reviews', None)
-        
-        if review_list != None:
+        website = detail.get("result", {}).get("website", None)
+        review_list = detail.get("result", {}).get("reviews", None)
+
+        if review_list is not None:
             newest_review = newest_review_date(review_list)
         else:
             newest_review = None
 
         place_info = {
-            "name":name,
-            "place_id":place_id,
-            "business_status":business_status,
-            "address":address,
-            "phone":phone,
-            "opening_hours":opening_hours,
-            "rating":rating,
-            "rating_total":rating_total,
-            "longitude":longitude,
-            "latitude":latitude,
-            "map_url":map_url,
-            "newest_review":newest_review
+            "name": name,
+            "place_id": place_id,
+            "business_status": business_status,
+            "address": address,
+            "phone": phone,
+            "opening_hours": opening_hours,
+            "types": types,
+            "rating": rating,
+            "rating_total": rating_total,
+            "longitude": longitude,
+            "latitude": latitude,
+            "map_url": map_url,
+            "website": website,
+            "newest_review": newest_review,
         }
     else:
         place_info = {
-            "name":ori_name,
-            "place_id":None,
-            "business_status":None,
-            "address":None,
-            "phone":None,
-            "opening_hours":None,
-            "rating":None,
-            "rating_total":None,
-            "longitude":None,
-            "latitude":None,
-            "map_url":None,
-            "newest_review":None
+            "name": ori_name,
+            "place_id": None,
+            "business_status": None,
+            "address": None,
+            "phone": None,
+            "opening_hours": None,
+            "types": None,
+            "rating": None,
+            "rating_total": None,
+            "longitude": None,
+            "latitude": None,
+            "map_url": None,
+            "website": None,
+            "newest_review": None,
         }
 
     return place_info
 
 
-def get_place_dict(name: str, address: str, api_key_path=None, api_key=None):
-    """帶入api_key.txt檔案路徑和「命稱」、「地址」字串，能自動搜尋並返回資訊。
-       返回資訊為字典，可直接轉換成DataFrame。"""
-    key_word = name + " " + address
+def get_place_dict(
+    name=None,
+    address=None,
+    keyword=None,
+    api_key_path=None,
+    api_key=None,
+    place_id=None,
+):
+    """輸入：key or key.txt 檔案路徑；名稱和地址or關鍵字or place_id。
+    會自動回傳地標資訊的字典"""
 
+    # 先確認key有輸入，有key才能使用服務
     if api_key_path is not None:
         api_key = get_key(api_key_path=api_key_path)
-    elif api_key is not None:
-        api_key = api_key
-    else:
-        print("please imput api_key or api_key_path")
-        return None
-        
-    place_id = get_place_id(api_key=api_key, key_word=key_word)
+    elif api_key is None:
+        raise TypeError("請輸入 API key 或 API key 路徑！")
+
+    # 接著確認用哪種方式進行搜尋
+    ## 如果有place id可直接精準搜尋回傳資訊
+    if place_id is not None:
+        place_dict = gmap_info(ori_name=name, api_key=api_key, place_id=place_id)
+        return place_dict
+
+    ## 若沒有place id，再確認是輸入關鍵字或是名字和地址
+    if keyword is None:
+        if name is None and address is None:
+            raise TypeError("請擇一輸入 keyword/name+address")
+        if name is None:
+            name = ""
+        if address is None:
+            address = ""
+
+        keyword = (name + " " + address).strip()
+
+    place_id = get_place_id(api_key=api_key, keyword=keyword)
     place_dict = gmap_info(ori_name=name, api_key=api_key, place_id=place_id)
-
     return place_dict
-
 
 
 def gmap_nearby_search(key, lat, lon, radius, keyword):
     """提供api_key、經緯度、搜尋半徑和關鍵字，回傳搜尋結果的列表，資訊包括名稱、place_id和營業狀態"""
     # 使用gmap連線並搜尋，取得搜尋結果
     gmaps = googlemaps.Client(key=key)
-    search_result = gmaps.places_nearby(location=(lat, lon), radius=radius, keyword=keyword)
+    search_result = gmaps.places_nearby(
+        location=(lat, lon), radius=radius, keyword=keyword
+    )
     result = search_result.get("results", [])
     page = 1
-    
+
     # 取出第一頁搜尋結果的名稱、place_id和地址，存成字典後加入list
     result_data = []
     for place in result:
         place_dict = {
-            "name":place.get("name", None),
-            "place_id":place.get("place_id", None),
-            "buss_status":place.get("business_status", None)
+            "name": place.get("name", None),
+            "place_id": place.get("place_id", None),
+            "buss_status": place.get("business_status", None),
         }
         result_data.append(place_dict)
-
 
     # 進入迴圈判斷是否有下一頁，如果沒有直接停止，若有則迴圈
     time.sleep(2.5)
     next_page = search_result.get("next_page_token", None)
-    
+
     while next_page:
         page += 1
         max_tries = 3
-        for tries in range(1, max_tries+1):
+        for tries in range(1, max_tries + 1):
             try:
                 print(f"正在取得第{page}頁資訊...")
                 time.sleep(2.5)
@@ -227,9 +276,9 @@ def gmap_nearby_search(key, lat, lon, radius, keyword):
                 next_page_result = next_page_search.get("results", [])
                 for place in next_page_result:
                     place_dict = {
-                        "name":place.get("name", None),
-                        "place_id":place.get("place_id", None),
-                        "buss_status":place.get("business_status", None)
+                        "name": place.get("name", None),
+                        "place_id": place.get("place_id", None),
+                        "buss_status": place.get("business_status", None),
                     }
                     result_data.append(place_dict)
                 time.sleep(2.5)
@@ -244,5 +293,5 @@ def gmap_nearby_search(key, lat, lon, radius, keyword):
                 else:
                     print(f"第{tries}次嘗試失敗：{e}\n等待3秒後重試...")
                     time.sleep(3)
-    
+
     return result_data
