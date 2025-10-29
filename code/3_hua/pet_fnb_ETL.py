ab86fc7adef747fd3089f63d4b5a73c9e2054580
Hua <huaworking@gmail.com>
2025-10-29 14:08:09 +0800

    修改內容

--- code/3_hua/pet_fnb_ETL.py
+++ code/3_hua/pet_fnb_ETL.py
@@ -37,8 +37,8 @@ df.insert(3, "city", df["address"].apply(city))
 # 將地址拆出鄉鎮市區欄位
 def district(addr):
     try :
-        c = re.search(r"(?:縣|市)(.{1,3}(鄉|鎮|市|區))", addr)
-        return c.group(1) if c else None
+        d = re.search(r"(?:縣|市)(.{1,3}(鄉|鎮|市|區))", addr)
+        return d.group(1) if d else None
     except Exception as e:
         print(f"有誤：{addr}（錯誤原因：{e}）")
         return None
@@ -47,4 +47,4 @@ df.insert(4, "district", df["address"].apply(district))
 
 
 # 將電話空值補上"unknown"
-df['phone'] = df['phone'].fillna('unknown').str.strip()
+df["phone"] = df["phone"].fillna("unknown").str.strip()
