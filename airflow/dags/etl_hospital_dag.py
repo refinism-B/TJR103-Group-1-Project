9db865883db078167fda240b2e72482a8a25775f
chgwyellow <dinnis1107@gmail.com>
2025-11-04 23:37:37 +0800

    <modify> - removed the useless .gitkeep and revised the dag.py to ensure match the multiple nods.

--- airflow/dags/etl_hospital_dag.py
+++ airflow/dags/etl_hospital_dag.py
@@ -1,7 +1,18 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from src.tasks import etl_hospital
+from tasks.hospital import (
+    E_hospital,
+    L_hospital,
+    T_hospital_c_d,
+    T_hospital_cat_id,
+    T_hospital_clean_sort,
+    T_hospital_details,
+    T_hospital_id,
+    T_hospital_merge,
+    T_hospital_place_id,
+    T_hospital_sql,
+)
 
 # -------------------------------------
 # ✨ Step 1. DAG 參數設定
@@ -32,9 +43,37 @@ with DAG(
     # -------------------------------------
     # ✨ Step 3. 定義 Airflow 任務
     # -------------------------------------
-    etl_task = PythonOperator(
-        task_id="etl_hospital_pipeline",
-        python_callable=etl_hospital.main,  # 直接呼叫整個 ETL 任務
+    extract = PythonOperator(task_id="extract", python_callable=E_hospital.main)
+    t_c_d = PythonOperator(
+        task_id="get_city_district", python_callable=T_hospital_c_d.main
     )
+    t_place_id = PythonOperator(
+        task_id="get_place_id", python_callable=T_hospital_place_id.main
+    )
+    t_details = PythonOperator(
+        task_id="get_details", python_callable=T_hospital_details.main
+    )
+    t_clean = PythonOperator(
+        task_id="clean_sort", python_callable=T_hospital_clean_sort.main
+    )
+    t_id = PythonOperator(task_id="add_id", python_callable=T_hospital_id.main)
+    t_merge = PythonOperator(task_id="merge", python_callable=T_hospital_merge.main)
+    t_cat_id = PythonOperator(
+        task_id="get_cat_id", python_callable=T_hospital_cat_id.main
+    )
+    t_sql = PythonOperator(task_id="final", python_callable=T_hospital_sql.main)
+    load = PythonOperator(task_id="load", python_callable=L_hospital.main)
 
-    etl_task
+    # 定義依賴關係（按順序連接）
+    (
+        extract
+        >> t_c_d
+        >> t_place_id
+        >> t_details
+        >> t_clean
+        >> t_id
+        >> t_merge
+        >> t_cat_id
+        >> t_sql
+        >> load
+    )
--- airflow/dags/etl_hotel_dag.py
+++ airflow/dags/etl_hotel_dag.py
@@ -1,7 +1,18 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from src.tasks import etl_hotel
+from tasks.hotel import (
+    E_hotel,
+    L_hotel,
+    T_hotel_c_d,
+    T_hotel_cat_id,
+    T_hotel_clean_sort,
+    T_hotel_details,
+    T_hotel_id,
+    T_hotel_merge,
+    T_hotel_place_id,
+    T_hotel_sql,
+)
 
 # -------------------------------------
 # ✨ Step 1. DAG 參數設定
@@ -32,9 +43,35 @@ with DAG(
     # -------------------------------------
     # ✨ Step 3. 定義 Airflow 任務
     # -------------------------------------
-    etl_task = PythonOperator(
-        task_id="etl_hospital_pipeline",
-        python_callable=etl_hotel.main,  # 直接呼叫整個 ETL 任務
+    extract = PythonOperator(task_id="extract", python_callable=E_hotel.main)
+    t_c_d = PythonOperator(
+        task_id="get_city_district", python_callable=T_hotel_c_d.main
     )
+    t_place_id = PythonOperator(
+        task_id="get_place_id", python_callable=T_hotel_place_id.main
+    )
+    t_details = PythonOperator(
+        task_id="get_details", python_callable=T_hotel_details.main
+    )
+    t_clean = PythonOperator(
+        task_id="clean_sort", python_callable=T_hotel_clean_sort.main
+    )
+    t_id = PythonOperator(task_id="add_id", python_callable=T_hotel_id.main)
+    t_merge = PythonOperator(task_id="merge", python_callable=T_hotel_merge.main)
+    t_cat_id = PythonOperator(task_id="get_cat_id", python_callable=T_hotel_cat_id.main)
+    t_sql = PythonOperator(task_id="final", python_callable=T_hotel_sql.main)
+    load = PythonOperator(task_id="load", python_callable=L_hotel.main)
 
-    etl_task
+    # 定義依賴關係（按順序連接）
+    (
+        extract
+        >> t_c_d
+        >> t_place_id
+        >> t_details
+        >> t_clean
+        >> t_id
+        >> t_merge
+        >> t_cat_id
+        >> t_sql
+        >> load
+    )
